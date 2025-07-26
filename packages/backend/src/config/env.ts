35d6526dab8bcc2c8b47c6322083fc28a62a64d5
Aryan <srivastavaryan03@gmail.com>
2025-07-26 23:37:14 +0530

    setup shared package and setup backend package with env system config

---
+++ packages/backend/src/config/env.ts
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import { z } from "zod";
+
+dotenv.config();
+
+const processEnvSchema = z.object({
+  PORT: z.coerce.number().default(5000),
+  NODE_ENV: z
+    .enum(["development", "production", "testing"])
+    .default("development"),
+});
+
+const result = processEnvSchema.safeParse(process.env);
+
+if (!result.success) {
+  console.log(`.env file error:- ${result.error.format}`);
+  process.exit(1);
+}
+
+export const env = result.data;
