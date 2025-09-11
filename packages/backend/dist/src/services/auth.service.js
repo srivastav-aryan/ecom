d4da80795f42a4ae0b9d69dc9e7ef596fcfac406
Aryan Srivastava <srivastavaryan03@gmail.com>
2025-09-11 19:05:59 +0530

    add login logic to auth service

--- packages/backend/dist/src/services/auth.service.js
+++ packages/backend/dist/src/services/auth.service.js
@@ -1,4 +1,6 @@
 import UserServices from "./user.service";
+import { User } from "../models/user.model";
+import { ApiError } from "../utilities/utilites";
 export default class AuthServices {
     static async registerUser(userInput) {
         const regUser = await UserServices.createUser(userInput);
@@ -7,5 +9,20 @@ export default class AuthServices {
         regUser.refreshToken = refreshToken;
         await regUser.save();
         return { refreshToken, accessToken };
+    }
+    static async loginUser(input) {
+        const { email, password } = input;
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            throw new ApiError(400, "sorry no such email exsists");
+        }
+        const passCheck = await user.isPasswordCorrect(password);
+        if (!passCheck) {
+            throw new ApiError(400, "Incorrect password");
+        }
+        const accToken = user.generateAccessToken();
+        const refToken = user.generateRefreshToken();
+        await user.save({ validateBeforeSave: false });
+        return { refToken, accToken };
     }
 }
--- packages/backend/src/services/auth.service.ts
+++ packages/backend/src/services/auth.service.ts
@@ -1,5 +1,7 @@
-import { userRegistrationInput } from "@e-com/shared/schemas";
+import { userLoginInput, userRegistrationInput } from "@e-com/shared/schemas";
 import UserServices from "./user.service";
+import { User } from "../models/user.model";
+import { ApiError } from "../utilities/utilites";
 
 export default class AuthServices {
   static async registerUser(userInput: userRegistrationInput) {
@@ -13,5 +15,28 @@ export default class AuthServices {
     await regUser.save();
 
     return { refreshToken, accessToken };
+  }
+
+  static async loginUser(input: userLoginInput) {
+    const { email, password } = input;
+
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      throw new ApiError(400, "sorry no such email exsists");
+    }
+
+    const passCheck = await user.isPasswordCorrect(password);
+
+    if (!passCheck) {
+      throw new ApiError(400, "Incorrect password");
+    }
+
+    const accToken = user.generateAccessToken();
+    const refToken = user.generateRefreshToken();
+
+    await user.save({ validateBeforeSave: false });
+
+    return { refToken, accToken };
   }
 }
