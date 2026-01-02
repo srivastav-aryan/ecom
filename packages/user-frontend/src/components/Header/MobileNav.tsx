9481405fee35157a622e94ebfc86a730ae7b7b27
Aryan Srivastava <srivastavaryan03@gmail.com>
2026-01-02 17:20:05 +0530

    improve mobile nav sheet

--- packages/user-frontend/src/components/Header/MobileNav.tsx
+++ packages/user-frontend/src/components/Header/MobileNav.tsx
@@ -1,11 +1,12 @@
 import {
   Sheet,
+  SheetFooter,
   SheetContent,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { Menu } from "lucide-react";
+import { FacebookIcon, InstagramIcon, Menu, TwitterIcon}from "lucide-react";
 import type { NavigationItem } from "@e-com/shared/types";
 import { Link } from "react-router-dom";
 import { useState } from "react";
@@ -35,46 +36,49 @@ function MobileNav({ navItems }: NavProps) {
       <SheetTrigger>
         <Menu />
       </SheetTrigger>
-      <SheetContent side="left" className=" overflow-hidden">
-        <SheetHeader className="border-b-3 border-black">
-          <SheetTitle>ECOM</SheetTitle>
+      <SheetContent side="left" className="bg-[rgb(28,28,28)] text-white overflow-hidden">
+        <SheetHeader className="border-b border-gray-600">
+          <SheetTitle className="text-white">ECOM</SheetTitle>
         </SheetHeader>
 
-        <div className="">
+        <div className="w-[90%] mx-auto border-b border-gray-600">
           {navItems.map((topItem) => {
             if (topItem.hasMenu) {
               return (
                 <div
                   key={topItem.id}
-                  className="py-2 px-4 cursor-pointer border-b-2 border-blue-200 flex justify-between"
+                  className="py-2  cursor-pointer  flex gap-2.5"
                   onClick={() => setActiveItem(topItem)}
                 >
-                  <p>{topItem.label}</p>
-                  <span className="text-gray-400">{">"}</span>
+                  <p className="text-[1.3rem]">{topItem.label}</p>
+                  <span className="text-white ">{">"}</span>
                 </div>
+
               );
-            } else {
+            }
+            else {
               return (
                 <Link
                   key={topItem.id}
-                  className="py-2 px-4 cursor-pointer border-b-2 border-blue-200  w-full block"
+                  className="py-2 text-[1.3rem] w-full block"
                   to={topItem.href}
                 >
                   {topItem.label}
                 </Link>
               );
             }
+
           })}
 
           <div
             className={`
-    absolute inset-0 bg-white mt-14
+    absolute inset-0  bg-[rgb(28,28,28)]  px-3 mt-20
     transition-transform duration-300 ease-out
     ${open && activeItem ? "translate-x-0" : "translate-x-full"}
   `}
           >
             <button
-              className="py-1 mb-2 border-black border rounded-4xl  px-5"
+              className="py-1 mb-6  bg-[rgb(55,55,55)] rounded-4xl text-sm px-2"
               onClick={() => setActiveItem(null)}
             >
               ← Back
@@ -82,17 +86,17 @@ function MobileNav({ navItems }: NavProps) {
 
             {activeItem?.hasMenu?.map((section) => (
               <Accordion type="single" collapsible className="w-full">
-                <AccordionItem value={section.name}>
-                  <AccordionTrigger className="border-2">{section.name}</AccordionTrigger>
+                <AccordionItem value={section.name} className=" ">
+                  <AccordionTrigger className="">{section.name}</AccordionTrigger>
                   <AccordionContent className="p-3 flex flex-col gap-4 text-balance">
                     {section.items.map((list) => (
                       <ul>
                         <li>
-                          <Link to={list.href} className="text-gray-500">{list.label}</Link>
+                          <Link to={list.href} className="text-gray-400 ">{list.label}</Link>
                           {!list.badge ? (
                             ""
                           ) : (
-                            <span className="ml-2 border-2 text-[0.7rem] rounded-full border-red-300 text-red-300 font-bold px-2 py-px">
+                            <span className="ml-2 border-2 text-[0.7rem] text-white rounded-full border-red-300  font-bold px-2 py-px">
                               {list.badge}
                             </span>
                           )}
@@ -105,6 +109,14 @@ function MobileNav({ navItems }: NavProps) {
             ))}
           </div>
         </div>
+
+        <SheetFooter className="border-gray-600 border-y">
+        <div className=" flex justify-between">
+          <FacebookIcon className="h-5 w-5" />
+          <InstagramIcon className="h-5 w-5"/>
+          <TwitterIcon className="h-5 w-5"/>
+        </div>
+        </SheetFooter>
       </SheetContent>
     </Sheet>
   );
