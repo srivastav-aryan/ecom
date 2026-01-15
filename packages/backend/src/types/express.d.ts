7abcf68b79c3b24faaea243ec9711cceb6b7c44b
Aryan Srivastava <srivastavaryan03@gmail.com>
2026-01-15 19:20:04 +0530

    change the req.user type defination

--- packages/backend/src/types/express.d.ts
+++ packages/backend/src/types/express.d.ts
@@ -1,12 +1,18 @@
+import { Types } from "mongoose";
 import type { IUser } from "../models/user.model";
 import type pino from "pino";
+import { UserRole } from "@e-com/shared/types";
 
 declare global {
   namespace Express {
     interface Request {
       id: string;
       log: pino.Logger;
-      user?: IUser;
+      user?: {
+        _id: Types.ObjectId,
+        email: string,
+        role: UserRole
+      };
     }
   }
 }
