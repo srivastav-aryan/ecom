01d042615159c496839e7da2ac24d385875fec20
Aryan Srivastava <srivastavaryan03@gmail.com>
2026-07-29 16:49:25 +0530

    bug: category route file was reaching directly into the composition file , not making use of DI pattern

--- packages/backend/src/modules/catalog/routes/category.routes.ts
+++ packages/backend/src/modules/catalog/routes/category.routes.ts
@@ -1,18 +1,24 @@
 import { PERMISSIONS } from "@e-com/shared/authorization";
-import { authenticateMiddlware } from "../../../composition/app.composition.js";
 import { authorize } from "../../../shared/middlewares/authorization.middleware.js";
 import { ICategoryContollerInterface } from "../interfaces/category.controller.interface.js";
-
 
 import express, { RequestHandler } from "express";
 import { validateReq } from "../../../shared/middlewares/validation.middleware.js";
 import { createCategorySchema } from "@e-com/shared/schemas";
 
-export const createCategoryRouter = (categoryController: ICategoryContollerInterface, authenticate: RequestHandler) => {
+export const createCategoryRouter = (
+  categoryController: ICategoryContollerInterface,
+  authenticate: RequestHandler,
+) => {
+  const categoryRouter = express.Router();
 
-  const categoryRouter = express.Router()
+  categoryRouter.post(
+    "/",
+    authenticate,
+    authorize(PERMISSIONS.CATEGORIES_CREATE),
+    validateReq(createCategorySchema),
+    categoryController.createCategory,
+  );
 
-  categoryRouter.post("/", authenticateMiddlware, authorize(PERMISSIONS.CATEGORIES_CREATE), validateReq(createCategorySchema), categoryController.createCategory)
-
-  return categoryRouter
-}
+  return categoryRouter;
+};
